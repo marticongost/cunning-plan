d941bdaae9ff3dbbdaf002ada6c0fc0b95713c49
mcongost <mcongost@guellconsulting.com>
2022-05-29 15:58:29 +0200

    Restyled the header

--- src/components/App.js
+++ src/components/App.js
@@ -20,24 +20,28 @@ function App() {
 
     return (
         <div className="App">
-            <h1 className="App-heading">Cunning plan</h1>
-            <Tabs>
-                <Tab name="attack-infantry" title="Atac a infanteria">
-                    <AttackSimulator />
-                </Tab>
-                <Tab name="attack-vehicle" title="Atac a vehicles">
-                    Catacroc
-                </Tab>
-                <Tab name="range-in" title="Artilleria">
-                    Spam
-                </Tab>
-                <Tab name="attack-close-quarters" title="Atac cos a cos">
-                    Zaaap
-                </Tab>
-                <Tab name="shock" title="Xoc">
-                    <ShockSimulator />
-                </Tab>
-            </Tabs>
+            <header className="App-header">
+                <h1 className="App-heading">Cunning plan</h1>
+            </header>
+            <main className="App-main">
+                <Tabs>
+                    <Tab name="attack-infantry" title="Atac a infanteria">
+                        <AttackSimulator />
+                    </Tab>
+                    <Tab name="attack-vehicle" title="Atac a vehicles">
+                        Catacroc
+                    </Tab>
+                    <Tab name="range-in" title="Artilleria">
+                        Spam
+                    </Tab>
+                    <Tab name="attack-close-quarters" title="Atac cos a cos">
+                        Zaaap
+                    </Tab>
+                    <Tab name="shock" title="Xoc">
+                        <ShockSimulator />
+                    </Tab>
+                </Tabs>
+            </main>
         </div>
     );
 }
