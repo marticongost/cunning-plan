76c02f46a926004950e3cd7d630541b427788d54
mcongost <mcongost@guellconsulting.com>
2022-06-05 11:48:55 +0200

    Added predictions to the initiative simulator

--- src/components/rolls/InitiativeSimulator.js
+++ src/components/rolls/InitiativeSimulator.js
@@ -1,5 +1,8 @@
 import React from "react";
 import RollSimulator from "./RollSimulator";
+import { ReactComponent as DelayIcon } from "../../svg/dice/delay.svg";
+import { ReactComponent as LightningIcon } from "../../svg/dice/lightning.svg";
+import { times } from "../../modules/utils";
 
 export default function InitiativeSimulator() {
     return (
@@ -9,6 +12,22 @@ export default function InitiativeSimulator() {
                     id: "initiative",
                     diceTypes: ["initiative"],
                 },
+            ]}
+            predictions={[
+                {
+                    title: "Iniciativa 0",
+                    icon: LightningIcon,
+                    amount: 1,
+                    test: (results) => !results.delay,
+                },
+                ...times(6, (n) => {
+                    return {
+                        title: "Iniciativa " + (n + 1),
+                        icon: DelayIcon,
+                        amount: n + 1,
+                        test: (results) => results.delay === n + 1,
+                    };
+                }),
             ]}
         />
     );
