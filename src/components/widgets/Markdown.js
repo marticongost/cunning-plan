ac4f2a6b2370f0b175b71d7a7edb84bc575924ba
mcongost <mcongost@guellconsulting.com>
2022-06-05 11:25:25 +0200

    Add a CSS class to the Markdown widget

--- src/components/widgets/Markdown.js
+++ src/components/widgets/Markdown.js
@@ -6,5 +6,7 @@ const markdownConverter = new Converter();
 export default function Markdown(props) {
     if (!props.content) return null;
     const html = markdownConverter.makeHtml(props.content);
-    return <div dangerouslySetInnerHTML={{ __html: html }} />;
+    return (
+        <div className="Markdown" dangerouslySetInnerHTML={{ __html: html }} />
+    );
 }
